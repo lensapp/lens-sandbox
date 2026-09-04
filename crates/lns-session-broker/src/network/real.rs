8ef1fa1cb3091f6f30e9ff76d3509efaa8146e01
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2026-09-04 14:56:05 +0000

    fix(session): keep DHCP leases active

--- crates/lns-session-broker/src/network/real.rs
+++ crates/lns-session-broker/src/network/real.rs
@@ -1,10 +1,18 @@
 #![cfg_attr(not(target_os = "linux"), allow(dead_code))]
 
 use std::os::unix::fs::PermissionsExt;
+#[cfg(target_os = "linux")]
+use std::sync::mpsc::{self, Receiver};
+#[cfg(target_os = "linux")]
+use std::thread::JoinHandle;
+#[cfg(target_os = "linux")]
+use std::time::Duration;
 
 use super::{CommandOutcome, CommandRunner, FsWriter};
 #[cfg(target_os = "linux")]
-use super::{DHCP_DNS_PATH, configure_dns_with, parse_dhcp_dns};
+use super::{
+    DHCP_DNS_PATH, KeepaliveWaiter, configure_dns_with, keep_dhcp_alive_with, parse_dhcp_dns,
+};
 
 #[derive(Default)]
 struct RealCommandRunner;
@@ -38,6 +46,49 @@ pub fn bring_up_eth0() -> Result<(), String> {
 pub fn configure_dns() -> Result<(), String> {
     let dhcp_dns = parse_dhcp_dns(&std::fs::read_to_string(DHCP_DNS_PATH).unwrap_or_default());
     configure_dns_with(&dhcp_dns, &RealFsWriter)
+}
+
+#[cfg(target_os = "linux")]
+struct StopWaiter {
+    stop: Receiver<()>,
+}
+
+#[cfg(target_os = "linux")]
+impl KeepaliveWaiter for StopWaiter {
+    fn wait(&self, duration: Duration) -> bool {
+        matches!(
+            self.stop.recv_timeout(duration),
+            Err(mpsc::RecvTimeoutError::Timeout)
+        )
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub struct DhcpKeepalive {
+    stop: mpsc::Sender<()>,
+    thread: Option<JoinHandle<()>>,
+}
+
+#[cfg(target_os = "linux")]
+impl Drop for DhcpKeepalive {
+    fn drop(&mut self) {
+        let _ = self.stop.send(());
+        if let Some(thread) = self.thread.take() {
+            let _ = thread.join();
+        }
+    }
+}
+
+#[cfg(target_os = "linux")]
+pub fn start_dhcp_keepalive() -> DhcpKeepalive {
+    let (stop, receiver) = mpsc::channel();
+    let thread = std::thread::spawn(move || {
+        keep_dhcp_alive_with(&RealCommandRunner, &StopWaiter { stop: receiver });
+    });
+    DhcpKeepalive {
+        stop,
+        thread: Some(thread),
+    }
 }
 
 #[cfg(test)]
