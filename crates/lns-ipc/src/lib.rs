20f243f9be6e3c48e4647de7afdd211d190e1bcc
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2026-09-09 20:15:14 +0000

    ci: reflow the merged export list

--- crates/lns-ipc/src/lib.rs
+++ crates/lns-ipc/src/lib.rs
@@ -27,10 +27,10 @@ pub use protocol::{
     ConnectorView, ContributionBlock, DisplacedEntry, ExecImageArgs, ImageInfo, ImageView,
     LogLevel, MixinView, MountSpec, PackedFilesetSource, PortPublish, Protocol, PushableImage,
     PushableLayer, RegistryLoginSummary, Request, Response, RunConfig, RunDetails, RunImageArgs,
-    RunStatsInfo, RunStatus, RunSummary, SandboxFileset,
-    SandboxFilesetOwner, SandboxMount, SandboxMountKind, SandboxPort, SandboxScript, SandboxView,
-    SaveKind, SecretValues, SessionTarget, SignalKind, SourceContribution, StatusInfo, VolumeInfo,
-    VolumeMount, VolumePruneFailure, cmdline_unsafe_char, validate_bind_source, validate_run_name,
+    RunStatsInfo, RunStatus, RunSummary, SandboxFileset, SandboxFilesetOwner, SandboxMount,
+    SandboxMountKind, SandboxPort, SandboxScript, SandboxView, SaveKind, SecretValues,
+    SessionTarget, SignalKind, SourceContribution, StatusInfo, VolumeInfo, VolumeMount,
+    VolumePruneFailure, cmdline_unsafe_char, validate_bind_source, validate_run_name,
     validate_volume_name, validate_volume_target,
 };
 pub use socket::{SocketPathError, default_socket_path};
