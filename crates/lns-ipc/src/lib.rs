d6ecd8ea6cea697c1592e6e92b806592143c6cc4
Hung-Han (Henry) Chen <hchen@mirantis.com>
2026-09-10 01:41:06 +0000

    style: the merged re-export list is wrapped as rustfmt wraps it

--- crates/lns-ipc/src/lib.rs
+++ crates/lns-ipc/src/lib.rs
@@ -24,13 +24,13 @@ pub use paths::{
 pub use protocol::{
     ApprovalAnswer, ApprovalEntryKind, ApprovalInfo, ArtifactInspection, BindMount, BindSpec,
     BuildContextFile, BuildSourceView, BuiltArchitecture, CachedKind, ConnectorConnectionView,
-    ConnectorMethodView, ConnectorView, ContributionBlock, DisplacedEntry, ExecImageArgs, ImageInfo,
-    ImageView, LogLevel, MixinView, MountSpec, PackedFilesetSource, PortPublish, Protocol,
-    PushableImage, PushableLayer, RegistryLoginSummary, Request, Response, RunConfig, RunDetails,
-    RunImageArgs, RunStatsInfo, RunStatus, RunSummary, SandboxFileset, SandboxFilesetOwner,
-    SandboxMount, SandboxMountKind, SandboxPort, SandboxScript, SandboxView, SaveKind, SecretValues,
-    SessionTarget, SignalKind, SourceContribution, StatusInfo, VolumeInfo, VolumeMount,
-    VolumePruneFailure, cmdline_unsafe_char, validate_bind_source, validate_run_name,
+    ConnectorMethodView, ConnectorView, ContributionBlock, DisplacedEntry, ExecImageArgs,
+    ImageInfo, ImageView, LogLevel, MixinView, MountSpec, PackedFilesetSource, PortPublish,
+    Protocol, PushableImage, PushableLayer, RegistryLoginSummary, Request, Response, RunConfig,
+    RunDetails, RunImageArgs, RunStatsInfo, RunStatus, RunSummary, SandboxFileset,
+    SandboxFilesetOwner, SandboxMount, SandboxMountKind, SandboxPort, SandboxScript, SandboxView,
+    SaveKind, SecretValues, SessionTarget, SignalKind, SourceContribution, StatusInfo, VolumeInfo,
+    VolumeMount, VolumePruneFailure, cmdline_unsafe_char, validate_bind_source, validate_run_name,
     validate_volume_name, validate_volume_target,
 };
 pub use socket::{SocketPathError, default_socket_path};
